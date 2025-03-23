84dfbccf27f156c611fbb04177ad54825fe29e23
Nicky Choi <32828004+njch5@users.noreply.github.com>
2025-03-22 20:47:57 -0700

    Upgrade Next.js, tailwind, and outstatic

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body suppressHydrationWarning={true} id="outstatic">{children}</body>
     </html>
   )
 }
